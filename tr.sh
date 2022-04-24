1597d6413ebbb6a06b9c8011c17f356277ca8e10
Jaanus Suurväli <8995770+jsuurvali@users.noreply.github.com>
2022-04-24 14:57:56 -0500

    Update tr.sh

--- tr.sh
+++ tr.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 #SBATCH --time=1:00:00
-#SBATCH --account=def-coling
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=40G
-#SBATCH --job-name=trnsl
 
 module load emboss
 
-cd /home/suurvalj/scratch/big_genomes/GCA_002915635.3_AmbMex60DD_genomic_big 
-ls *.fa | sed 's/\.fa//' | while read i; do echo $i; transeq -sequence $i.fa -outseq $i.protein.fa -frame 6 -clean; done
+# This script reads in all *.fa files in a folder and translates them in six reading frames
+# All stop codons are replaced with the character X
+# The output files are named *.protein.fa
+# The "#SBATCH" and "module load" lines are for running the script on SLURM
 
-cd /home/suurvalj/scratch/big_genomes/GCA_016271365.1_neoFor_v3_genomic_big
+cd /home/suurvalj/scratch/big_genomes      # replace this with actual location of the files
 ls *.fa | sed 's/\.fa//' | while read i; do echo $i; transeq -sequence $i.fa -outseq $i.protein.fa -frame 6 -clean; done
