1cf72c0c94b0006cbc2ececc4a2acb541e51afdf
Jaanus Suurväli <8995770+jsuurvali@users.noreply.github.com>
2022-04-24 15:12:35 -0500

    Update gc.sh

--- gc.sh
+++ gc.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-#SBATCH --time=1:00:00
-#SBATCH --account=def-coling
-#SBATCH --cpus-per-task=1
-#SBATCH --mem=24G
+# calculates the GC content of a set of files that end with *fna (genome sequences from the NCBI databases).
+# The first 13 characters of each file name are used to label the output.
 
-cd /home/suurvalj/scratch/genomes
-ls *fna | cut -c-13 | while read asm; do grep -v ">" ${asm}*fna | awk -v x="${asm}" 'BEGIN {FS=""; gc = 0 ; at = 0} {if ($1 ~ /[gcGC]/) gc++ ; else if ($1 ~ /[atAT]/) at++} END {print x" "gc/(gc+at)}' >> gc.tsv; done
+ls *fna | cut -c-13 | while read asm; do \
+  grep -v ">" ${asm}*fna | \
+  awk -v x="${asm}" 'BEGIN {FS=""; gc = 0 ; at = 0} {if ($1 ~ /[gcGC]/) gc++ ; else if ($1 ~ /[atAT]/) at++} END {print x" "gc/(gc+at)}' \
+  >> gc.tsv
+done
